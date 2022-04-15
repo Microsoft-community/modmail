8ea477d894492614bba2cd9a111f59cb654ef823
Erisa A <erisa@erisa.uk>
2022-04-15 00:18:55 +0000

    Apply formatting changes

--- cogs/modmail.py
+++ cogs/modmail.py
@@ -2036,7 +2036,11 @@ class Modmail(commands.Cog):
         embed = None
 
         if self.bot.config["raid_mode"]:
-            embed = discord.Embed(title="Raid Mode currently enabled", color=self.bot.main_color, description=self.bot.config["raid_mode_message"])
+            embed = discord.Embed(
+                title="Raid Mode currently enabled",
+                color=self.bot.main_color,
+                description=self.bot.config["raid_mode_message"],
+            )
         else:
             embed = discord.Embed(
                 title="Raid Mode currently disabled",
